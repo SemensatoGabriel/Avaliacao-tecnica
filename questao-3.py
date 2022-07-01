e724c75967b1cac2022c6ffcd2603e23af5e57c5
SemensatoGabriel <semensatogabriel@gmail.com>
2022-06-30 22:25:00 -0300

    Create questao-3.py

---
+++ questao-3.py
@@ -0,0 +1,85 @@
+import requests
+
+#CONSULTANDO A API DO STAR WARS - PERSONAGENS
+lista = []
+num = 1
+''' 
+    Criando uma lista vazia para armazenamento dos dados verdadeiros e
+    uma variavel de contagem auxiliar.
+'''
+
+while num <= 9:
+    ''' 
+        A consulta na API referente a "people" contém 9 páginas, 
+        utilizei o while para a leitura de todas as paginas.
+    '''
+    peoples = requests.get("https://swapi.dev/api/people/?page=%d" % (num))
+    peoples = peoples.json()
+
+    peoples = peoples['results']
+
+    num2 = 0
+    while num2 < len(peoples):
+        ''' 
+            Verificando na página a condição se o personagem
+            participou de mais de 4 filmes.
+        '''
+
+        if len(peoples[num2]['films']) >= 4:
+            ''' Se verdadeiro armazena o nome na lista e incrementa 1 na contagem'''
+            nome = str(peoples[num2]['name'])
+            lista.append(nome)
+            num2 = num2 + 1
+
+        else:
+            ''' Se falso apenas incrementa 1 na contagem'''
+            num2 = num2 + 1
+
+    ''' Após executar 1 página, incrementa 1 para verificar a prox página'''
+    num = num + 1
+
+#APRESENTANDO A RESPOSTA
+print("Os personagens que aparecem em 4 ou mais filmes são: ", lista)
+
+
+
+
+#CONSULTANDO A API DO STAR WARS - PERSONAGENS
+lista2 = []
+num3 = 1
+''' 
+    Criando uma lista vazia para armazenamento dos dados verdadeiros e
+    uma variavel de contagem auxiliar.
+'''
+
+while num3 <= 6:
+    ''' 
+        A consulta referente a "planets" contém 6 páginas, 
+    '''
+    planets = requests.get("https://swapi.dev/api/planets/?page=%d" % (num3))
+    planets = planets.json()
+
+    planets = planets['results']
+
+    num4 = 0
+    while num4 < len(planets):
+        ''' 
+            Verificando na página a condição se o planeta
+            possui pelo menos 5 moradores.
+        '''
+
+        if len(planets[num4]['residents']) >= 5:
+            ''' Se verdadeiro armazena o nome na lista e incrementa 1 na contagem'''
+            nome = str(planets[num4]['name'])
+            lista2.append(nome)
+            num4 = num4 + 1
+
+        else:
+            ''' Se falso apenas incrementa 1 na contagem'''
+            num4 = num4 + 1
+
+    ''' Após executar 1 página, incrementa 1 para verificar a prox página'''
+    num3 = num3 + 1
+
+#APRESENTANDO A RESPOSTA
+print("Os planetas que possuem 5 ou mais moradores são: ", lista2)
